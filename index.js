89fe8ed9fe826fdb38a124f0527e20e497ee8b12
Bergkley <bergkley@gmail.com>
2024-07-18 20:41:08 -0300

    feat:  criando rota de teste

--- index.js
+++ index.js
@@ -5,5 +5,10 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//rotas
+app.get('/', (req, res) => {
+    res.json({ message: 'API funcionando!' });
+});
+
 
 app.listen(3000, () => console.log('Server running on port 3000'));

2936b7764e52102a865b15790c3150c808d41137
Bergkley <bergkley@gmail.com>
2024-07-19 21:51:26 -0300

    feat: configurando a rota

--- index.js
+++ index.js
@@ -10,28 +10,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //rotas api
-app.post('/person', async(req, res) => {
-  const { name, salary, approved } = req.body;
 
-  if(!name){
-    res.status(422).json({ message: 'O nome e obrigatorio!' });	
-  }
-
-  const person ={
-    name,
-    salary,
-    approved
-  }
-
-  try {
-    await Person.create(person);
-    res.status(201).json({ message: 'Pessoa inserida com sucesso!' });
-  } catch (error) {
-    res.status(500).json({ message: error });
-  }
-
- 
-})
+const personRoutes = require("./routes/personRoutes");
+app.use("/person", personRoutes);
 
 //rotas
 app.get("/", (req, res) => {

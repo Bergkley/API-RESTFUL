2f957caad5265d468a006009df6fef674c64eaf8
Bergkley <bergkley@gmail.com>
2024-07-19 21:50:50 -0300

    feat: ceinado a primeira rota

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -1,2 +1,25 @@
 const router = require('express').Router();
 
+router.post('/person', async(req, res) => {
+    const { name, salary, approved } = req.body;
+  
+    if(!name){
+      res.status(422).json({ message: 'O nome e obrigatorio!' });	
+    }
+  
+    const person ={
+      name,
+      salary,
+      approved
+    }
+  
+    try {
+      await Person.create(person);
+      res.status(201).json({ message: 'Pessoa inserida com sucesso!' });
+    } catch (error) {
+      res.status(500).json({ message: error });
+    }
+  
+   
+  })
+  module.exports = router
